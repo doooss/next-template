4cabd668e96ac5367f5d3727750bd85d3699293b
doyoung <ldy9212@gmail.com>
2022-07-12 21:32:10 +0900

    [07-12] clear

--- src/components/elements/Ripple/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './Ripple';
-export { default } from './Ripple';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,5 @@
-import type { NextPage } from 'next';
-
-import { GridContainer, GridItem } from '../components/elements/Grid';
-
-const Home: NextPage = () => {
-  return (
-    <GridContainer width="100%">
-      <GridItem mobile={6} tablet={4} row={3}>
-        Next Template post css ver
-      </GridItem>
-      <GridItem mobile={6} tablet={4} row={3}>
-        Next Template post css ver
-      </GridItem>
-      <GridItem mobile={6} tablet={4} row={3}>
-        Next Template post css ver
-      </GridItem>
-      <GridItem mobile={6} tablet={4} row={3}>
-        Next Template post css ver
-      </GridItem>
-    </GridContainer>
-  );
+const Home = () => {
+    return <div>postcss - jest - prettier - eslint -next template</div>;
 };
 
 export default Home;
